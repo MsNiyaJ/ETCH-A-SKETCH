7e9e553e0df171d758c2658b6312e3a653d39f2f
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-08-25 15:51:17 -0400

    add a hover effect to each box

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const board = document.querySelector('.board');
 //Create 256 boxes and insert them into the board div
 for(let i = 0; i < 256; i++){
     let box = document.createElement('div');
-    box.style.border = 'grey 1px solid';
-    box.style.backgroundColor = 'white';
-    box.style.width = '50px';
-    box.style.height = '50px';
+    box.classList.add('box');
     board.appendChild(box);
 }
