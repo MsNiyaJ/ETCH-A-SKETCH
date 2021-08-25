5579b2fe00174ec9e06ece3761a7474bf7d2e15e
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-08-25 16:09:31 -0400

    onhover the background color of the box changes

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ for(let i = 0; i < 256; i++){
     box.classList.add('box');
     board.appendChild(box);
 }
+
+const boxes = document.querySelectorAll('.box');
+
+//Adds a hover event to each box, onhover it's background color will change
+boxes.forEach(box => {
+    box.addEventListener("mouseenter", function( event ) {
+        // the mouseenter target will become orange
+        event.target.style.backgroundColor = "orange";
+      });
+});
